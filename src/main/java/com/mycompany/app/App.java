15d9eafb5d77aa5fe1186269beb0972d73caeac2
wooneeloi <wooneeloi@gmail.com>
2025-04-10 14:53:28 +0800

    Simulating a stupid error6 !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is soooo boring!");
+        System.out.println("Jenkins is soooo boring! test");
     }
 
     public String getMessage() {
