2fc14ae2a7867d1f4c50b9e25019b3ce4250f536
wooneeloi <wooneeloi@gmail.com>
2025-04-10 14:19:39 +0800

    Simulating a stupid error5 !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I love Jenkins1";
+    private static final String MESSAGE = "I love Jenkins";
 
     public App() {}
 
